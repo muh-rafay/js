4b7c9e60fb81a24b76c0c59e60e4708f16152021
Muhammad Rafay <muhammadrafay36@gmail.com>
2022-05-10 18:07:31 +0500

    first push

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  $('.red-box').fadeTo(0,0.5);
+  $('.green-box').fadeTo(0,0.5);
+  $('.blue-box').fadeTo(0,0.5);
+});
